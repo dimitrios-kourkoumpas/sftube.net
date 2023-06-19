f15f671656a38cf63bbd9eb255a50f94d4a9021f
Dimitrios Kourkoumps <dkourk@gmail.com>
2023-06-19 16:20:32 +0200

    Configurations service

---
+++ src/Service/Configurations.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\ConfigurationRepository;
+
+/**
+ * Class Configurations
+ * @package App\Service
+ */
+final class Configurations
+{
+    /**
+     * @var \stdClass|null $configurations
+     */
+    private ?\stdClass $configurations = null;
+
+    /**
+     * @param ConfigurationRepository $repository
+     */
+    public function __construct(ConfigurationRepository $repository)
+    {
+        $configurations = $repository->findAll();
+
+        $this->configurations = new \stdClass();
+
+        foreach ($configurations as $configuration) {
+            $this->configurations->{$configuration->getName()} = $configuration->getValue();
+        }
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function isSet(string $name): bool
+    {
+        return isset($this->configurations->{$name});
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function get(string $name): string
+    {
+        return $this->configurations->{$name};
+    }
+}
