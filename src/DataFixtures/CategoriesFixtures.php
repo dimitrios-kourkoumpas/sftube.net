d8c75a9f7027b66d098561f5ee08ec607f80e124
Dimitrios Kourkoumps <dkourk@gmail.com>
2023-06-20 16:42:31 +0200

    Categories fixtures and datafile

---
+++ src/DataFixtures/CategoriesFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DataFixtures\Loader\DataLoader;
+use App\DataFixtures\Reader\JSONDataReader;
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+/**
+ * Class CategoriesFixtures
+ * @package App\DataFixtures
+ */
+final class CategoriesFixtures extends Fixture
+{
+    private const DATA_FILE = 'categories.json';
+
+    /**
+     * @param ParameterBagInterface $parameters
+     */
+    public function __construct(private readonly ParameterBagInterface $parameters)
+    {
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @return void
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $categories = DataLoader::loadData(
+            new JSONDataReader(
+                $this->parameters->get('app.fixtures.datapath') . DIRECTORY_SEPARATOR . self::DATA_FILE
+            )
+        );
+
+        foreach ($categories as $cat) {
+            $category = new Category();
+
+            $category->setName($cat['name']);
+
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+}
