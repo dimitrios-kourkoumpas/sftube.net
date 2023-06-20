bdf86767427d3ffb397c7add55982c3d00bbc781
Dimitrios Kourkoumps <dkourk@gmail.com>
2023-06-20 16:47:07 +0200

    Tags datafixtures and datafile

---
+++ src/DataFixtures/TagsFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DataFixtures\Loader\DataLoader;
+use App\DataFixtures\Reader\JSONDataReader;
+use App\Entity\Tag;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+/**
+ * Class TagsFixtures
+ * @package App\DataFixtures
+ */
+final class TagsFixtures extends Fixture
+{
+    private const DATA_FILE = 'tags.json';
+
+    /**
+     * @param ParameterBagInterface $parameters
+     */
+    public function __construct(private readonly ParameterBagInterface $parameters)
+    {
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @return void
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $tags = DataLoader::loadData(
+            new JSONDataReader(
+                $this->parameters->get('app.fixtures.datapath') . DIRECTORY_SEPARATOR . self::DATA_FILE
+            )
+        );
+
+        foreach ($tags as $t) {
+            $tag = new Tag();
+
+            $tag->setName($t['name']);
+
+            $manager->persist($tag);
+        }
+
+        $manager->flush();
+    }
+}
