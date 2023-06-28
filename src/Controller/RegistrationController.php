2e9c4c0f71f9358f73339558a61c44e2a950b416
Dimitrios Kourkoumps <dkourk@gmail.com>
2023-06-28 11:24:12 +0200

    User registration

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Security\LoginFormAuthenticator;
+use App\Service\UserRegistration;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+/**
+ * Class RegistrationController
+ * @package App\Controller
+ */
+final class RegistrationController extends BaseController
+{
+    #[Route('/register', name: 'app.register', methods: ['GET', 'POST'])]
+    #[IsGranted('register')]
+    public function register(Request $request, UserRegistration $registration, Security $security): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $registration->register($user);
+
+            $this->addFlash('success', $this->translator->trans('controller.registration.register.flash.registration-successful'));
+
+            return $security->login($user, 'form_login', 'main');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
