3553d20f365896e3eb563496c66b5d28d36d4b02
Dimitrios Kourkoumpas <dkourk@gmail.com>
2023-07-25 20:58:19 +0300

    Doctrine extensions

---
+++ src/Doctrine/Extension/RegularUsersExtension.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Doctrine\Extension;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Class RegularUsersExtension
+ * @package App\Doctrine\Extension
+ */
+final class RegularUsersExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param QueryNameGeneratorInterface $queryNameGenerator
+     * @param string $resourceClass
+     * @param Operation|null $operation
+     * @param array $context
+     * @return void
+     */
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
+    {
+        $em = $queryBuilder->getEntityManager();
+
+        $filters =  $em->getFilters();
+
+        if ($resourceClass === User::class) {
+            $filters->enable('regularUsersFilter');
+        }
+    }
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param QueryNameGeneratorInterface $queryNameGenerator
+     * @param string $resourceClass
+     * @param array $identifiers
+     * @param Operation|null $operation
+     * @param array $context
+     * @return void
+     */
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, Operation $operation = null, array $context = []): void
+    {
+        // TODO: Implement applyToItem() method.
+    }
+}
