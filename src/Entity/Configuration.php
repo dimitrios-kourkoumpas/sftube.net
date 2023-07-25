85033179ff24d6911f868eecd0f800ec81a308f6
Dimitrios Kourkoumpas <dkourk@gmail.com>
2023-07-25 21:16:31 +0300

    Security

--- src/Entity/Configuration.php
+++ src/Entity/Configuration.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\ConfigurationRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
@@ -24,6 +28,18 @@ use Symfony\Component\Validator\Constraints as Assert;
     operations: [
         new GetCollection(),
         new Get(),
+        new Post(
+            security: 'is_granted(\'' . User::ROLE_ADMIN . '\')'
+        ),
+        new Put(
+            security: 'is_granted(\'' . User::ROLE_ADMIN . '\')'
+        ),
+        new Patch(
+            security: 'is_granted(\'' . User::ROLE_ADMIN . '\')'
+        ),
+        new Delete(
+            security: 'is_granted(\'' . User::ROLE_ADMIN . '\')'
+        ),
     ]
 )]
 class Configuration
